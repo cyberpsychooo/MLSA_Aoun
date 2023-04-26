10dc993b651e0d38ca00d08504486d1f617c6c4e
cyberpsychooo <96617876+cyberpsychooo@users.noreply.github.com>
2023-04-26 10:05:03 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/b1.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "boy standing ";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Cyber Security student studying at Air University. I enjoy penetration testing and digital forensics.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Penetration testing",
+  "Network security",
+  "Digital forensics",
+  "Cyber security",
+  "Ethical hacking",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about making the cyber world more secure. I continually look for new and better ways to make tech secure for all.";
 
 const About = () => {
   return (
